867c608778aae5f26686b48272884a1ec6f925a2
Martín Calderón <martincalderon92@outlook.com>
2021-10-20 10:49:39 -0300

    ♻️ Delete excess handler.

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -4,10 +4,6 @@ import Spinner from './Spinner';
 import Card from './Card';
 
 export default function CardsContainer({ loading, posts, toDetail }) {
-	const handleToDetail = (postId) => {
-		toDetail(postId);
-	};
-
 	return (
 		<div className={styles.cardsContainer}>
 			{loading && <Spinner />}
@@ -19,7 +15,7 @@ export default function CardsContainer({ loading, posts, toDetail }) {
 						key={`card${post.id}`}
 						name={post.name}
 						image={post.cover_art?.formats?.small?.url}
-						onClick={() => handleToDetail(post.id)}
+						onClick={() => toDetail(post.id)}
 					/>
 				))}
 		</div>
