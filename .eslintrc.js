47a0a3b410fdbd690da5277bf78cb5244e99e7a9
Martín Calderón <martincalderon92@outlook.com>
2021-10-20 20:10:37 -0300

    ➕ Add TypeScript ESlint dependencies and update ESlint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
 		es2021: true,
 	},
 	extends: ['plugin:react/recommended'],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
@@ -11,7 +12,7 @@ module.exports = {
 		ecmaVersion: 12,
 		sourceType: 'module',
 	},
-	plugins: ['react'],
+	plugins: ['react', '@typescript-eslint'],
 	rules: {
 		quotes: ['error', 'single', { allowTemplateLiterals: true }],
 		'no-console': ['error', { allow: ['warn', 'error'] }],
