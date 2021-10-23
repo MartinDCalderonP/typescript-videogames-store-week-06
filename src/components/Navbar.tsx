8c0b0fbe3eb39ad825f9a1be4f688b9c8c6956d7
Martín Calderón <martincalderon92@outlook.com>
2021-10-22 22:38:55 -0300

    :fire: Remove unused Goback.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from '../styles/Navbar.module.scss';
 import { INavbar } from '../interfaces/types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faHome,
-	faArrowAltCircleLeft,
 	faSignInAlt,
 	faUserCircle,
 } from '@fortawesome/free-solid-svg-icons';
@@ -13,11 +12,6 @@ import Modal from './Modal';
 
 export default function Navbar({ onLoggedUser, user }: INavbar) {
 	const [openModal, setOpenModal] = useState(false);
-	const history = useHistory();
-
-	const toPreviousPage = () => {
-		history.goBack();
-	};
 
 	const handleSignInClick = () => {
 		setOpenModal(true);
@@ -40,14 +34,6 @@ export default function Navbar({ onLoggedUser, user }: INavbar) {
 	return (
 		<>
 			<nav className={styles.navbar}>
-				<div onClick={toPreviousPage}>
-					<FontAwesomeIcon
-						className={styles.goBack}
-						icon={faArrowAltCircleLeft}
-					/>
-					Go Back
-				</div>
-
 				<Link to="/">
 					<FontAwesomeIcon className={styles.home} icon={faHome} />
 					Home
